617572cd33ed1f81e26a6bb0ec8663728e2fbc99
riqi3 <enrique.pacudan@usjr.edu.ph>
2023-02-04 01:36:27 +0800

    adding constants
    color

---
+++ food_app/lib/constants.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+const mainColor = Color(0xFF010507);
+const activeColor = Color(0xFF22a45d);
+const accentColor = Color(0xFFef9920);
+const bodyTextColor = Color(0xFF868686);
+const inputColor = Color(0xFFfbfbfb);
+const bgColor = Colors.white;
+
+const double defPad = 16.0;
---
+++ food_app/lib/screens/home/data.dart
@@ -0,0 +1,3 @@
+List<String> images=[
+  
+];
